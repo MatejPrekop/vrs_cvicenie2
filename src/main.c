7c066ca63932800f2079b27542818037a4182707
Mallto <matej.prekop@gmail.com>
2016-10-11 14:27:00 +0200

    uloha1
    NASTAVENIE PERIFERIE LED

--- src/main.c
+++ src/main.c
@@ -107,6 +107,18 @@ int main(void)
 		}
 	}
 
+	void setLed(void) {
+		RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+		GPIO_InitTypeDef gpioInitStruc;
+
+		gpioInitStruc.GPIO_Pin = GPIO_Pin_5;
+		gpioInitStruc.GPIO_Mode = GPIO_Mode_OUT;
+		gpioInitStruc.GPIO_OType = GPIO_OType_PP;
+		gpioInitStruc.GPIO_PuPd = GPIO_PuPd_UP;
+		gpioInitStruc.GPIO_Speed = GPIO_Speed_40MHz;
+		GPIO_Init(GPIOA, &gpioInitStruc);
+	}
+
 
   /* Infinite loop */
 	while (1) {
